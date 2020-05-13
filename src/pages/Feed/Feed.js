f9638e0651c4434ec6c17b41a80e916d0eb19825
virghiu@Virjhiu <virghiu@gmail.com>
2020-05-13 17:47:53 -0400

    send image in FormData

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -106,13 +106,19 @@ class Feed extends Component {
     this.setState({
       editLoading: true,
     });
-    // Set up data (with image!)
-    let url = 'URL';
+    const formData = new FormData();
+    formData.append('title', postData.title);
+    formData.append('content', postData.content);
+    formData.append('image', postData.image);
+    let url = 'http://localhost:8080/feed/post';
     if (this.state.editPost) {
       url = 'URL';
     }
 
-    fetch(url)
+    fetch(url, {
+      method: 'POST',
+      body: formData,
+    })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Creating or editing a post failed!');
