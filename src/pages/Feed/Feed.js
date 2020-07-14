cce521c6fba4747f06551535ae59969c0867e18f
virghiu <virghiu@gmail.com>
2020-07-14 15:57:32 -0400

    post create

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -126,38 +126,56 @@ class Feed extends Component {
     this.setState({
       editLoading: true,
     });
-    console.log(postData);
+
     const formData = new FormData();
     formData.append('title', postData.title);
     formData.append('content', postData.content);
     formData.append('image', postData.image);
-    let url = 'http://localhost:8080/feed/post';
-    let method = 'POST';
-    if (this.state.editPost) {
-      url = 'http://localhost:8080/feed/post/' + this.state.editPost._id;
-      method = 'PUT';
-    }
 
-    fetch(url, {
-      method: method,
-      body: formData,
+    let graphqlQuery = {
+      query: `
+      mutation {
+        createPost(postInput: {title: "${postData.title}", content: "${postData.content}", imageUrl: "some image url"}) {
+          _id
+          title
+          content
+          imageUrl
+          creator {
+            name
+          }
+          createdAt
+        }
+      }
+      `,
+    };
+
+    fetch('http://localhost:8080/graphql', {
+      method: 'POST',
+      body: JSON.stringify(graphqlQuery),
       headers: {
         Authorization: 'Bearer ' + this.props.token,
+        'Content-Type': 'application/json',
       },
     })
       .then((res) => {
-        if (res.status !== 200 && res.status !== 201) {
-          throw new Error('Creating or editing a post failed!');
-        }
         return res.json();
       })
-      .then((resData) => {
+      .then(({ data }) => {
+        if (data.errors && data.errors[0].status === 422) {
+          throw new Error(
+            "Validation failed. Make sure the email address isn't used yet!"
+          );
+        }
+        if (data.errors) {
+          throw new Error('User creation failed!');
+        }
+        console.log(data);
         const post = {
-          _id: resData.post._id,
-          title: resData.post.title,
-          content: resData.post.content,
-          creator: resData.post.creator,
-          createdAt: resData.post.createdAt,
+          _id: data.createPost._id,
+          title: data.createPost.title,
+          content: data.createPost.content,
+          creator: data.createPost.creator,
+          createdAt: data.createPost.createdAt,
         };
         this.setState((prevState) => {
           return {
